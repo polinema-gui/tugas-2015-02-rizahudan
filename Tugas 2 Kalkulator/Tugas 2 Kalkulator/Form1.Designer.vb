b1cebb1a0ba65e97c8fd91bd301d1bfa3385c8ab
rizahudan <rizahudan@gmail.com>
2015-10-03 14:01:02 +0700

    Tugas 2 Tentang Kalkulator

---
+++ Tugas 2 Kalkulator/Tugas 2 Kalkulator/Form1.Designer.vb
@@ -0,0 +1,223 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.layarKalkulator = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button0 = New System.Windows.Forms.Button()
+        Me.samadenganButton = New System.Windows.Forms.Button()
+        Me.clearButton = New System.Windows.Forms.Button()
+        Me.kurangButton = New System.Windows.Forms.Button()
+        Me.tambahButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'layarKalkulator
+        '
+        Me.layarKalkulator.Location = New System.Drawing.Point(12, 13)
+        Me.layarKalkulator.Multiline = True
+        Me.layarKalkulator.Name = "layarKalkulator"
+        Me.layarKalkulator.ReadOnly = True
+        Me.layarKalkulator.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.layarKalkulator.Size = New System.Drawing.Size(178, 20)
+        Me.layarKalkulator.TabIndex = 0
+        Me.layarKalkulator.Text = "0"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 131)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(40, 40)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(58, 131)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(40, 40)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(104, 131)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(40, 40)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(12, 85)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(40, 40)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(58, 85)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(40, 40)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(104, 85)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(40, 40)
+        Me.Button6.TabIndex = 6
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(12, 39)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(40, 40)
+        Me.Button7.TabIndex = 7
+        Me.Button7.Text = "7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(58, 39)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(40, 40)
+        Me.Button8.TabIndex = 8
+        Me.Button8.Text = "8"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(104, 39)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(40, 40)
+        Me.Button9.TabIndex = 9
+        Me.Button9.Text = "9"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button0
+        '
+        Me.Button0.Location = New System.Drawing.Point(12, 177)
+        Me.Button0.Name = "Button0"
+        Me.Button0.Size = New System.Drawing.Size(40, 40)
+        Me.Button0.TabIndex = 10
+        Me.Button0.Text = "0"
+        Me.Button0.UseVisualStyleBackColor = True
+        '
+        'samadenganButton
+        '
+        Me.samadenganButton.Location = New System.Drawing.Point(58, 177)
+        Me.samadenganButton.Name = "samadenganButton"
+        Me.samadenganButton.Size = New System.Drawing.Size(86, 40)
+        Me.samadenganButton.TabIndex = 11
+        Me.samadenganButton.Text = "="
+        Me.samadenganButton.UseVisualStyleBackColor = True
+        '
+        'clearButton
+        '
+        Me.clearButton.Location = New System.Drawing.Point(150, 39)
+        Me.clearButton.Name = "clearButton"
+        Me.clearButton.Size = New System.Drawing.Size(40, 86)
+        Me.clearButton.TabIndex = 12
+        Me.clearButton.Text = "Clear"
+        Me.clearButton.UseVisualStyleBackColor = True
+        '
+        'kurangButton
+        '
+        Me.kurangButton.Location = New System.Drawing.Point(150, 131)
+        Me.kurangButton.Name = "kurangButton"
+        Me.kurangButton.Size = New System.Drawing.Size(40, 40)
+        Me.kurangButton.TabIndex = 13
+        Me.kurangButton.Text = "-"
+        Me.kurangButton.UseVisualStyleBackColor = True
+        '
+        'tambahButton
+        '
+        Me.tambahButton.Location = New System.Drawing.Point(150, 177)
+        Me.tambahButton.Name = "tambahButton"
+        Me.tambahButton.Size = New System.Drawing.Size(40, 40)
+        Me.tambahButton.TabIndex = 14
+        Me.tambahButton.Text = "+"
+        Me.tambahButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(198, 228)
+        Me.Controls.Add(Me.tambahButton)
+        Me.Controls.Add(Me.kurangButton)
+        Me.Controls.Add(Me.clearButton)
+        Me.Controls.Add(Me.samadenganButton)
+        Me.Controls.Add(Me.Button0)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.layarKalkulator)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents layarKalkulator As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Button0 As System.Windows.Forms.Button
+    Friend WithEvents samadenganButton As System.Windows.Forms.Button
+    Friend WithEvents clearButton As System.Windows.Forms.Button
+    Friend WithEvents kurangButton As System.Windows.Forms.Button
+    Friend WithEvents tambahButton As System.Windows.Forms.Button
+
+End Class
---
+++ Tugas 2 Kalkulator/Tugas 2 Kalkulator/Form1.vb
@@ -0,0 +1,147 @@
+﻿Public Class Form1
+
+    Dim x As Integer
+    Dim cek As Byte
+    Dim cekOnce As Boolean = False
+
+    Private Sub Button0_Click(sender As Object, e As EventArgs) Handles Button0.Click
+        If (cekIsi()) Then
+            layarKalkulator.Text = "0"
+        Else
+            layarKalkulator.Text &= "0"
+        End If
+        Me.cekOnce = True
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If (cekIsi()) Then
+            layarKalkulator.Text = "1"
+        Else
+            layarKalkulator.Text &= "1"
+        End If
+        Me.cekOnce = True
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If (cekIsi()) Then
+            layarKalkulator.Text = "2"
+        Else
+            layarKalkulator.Text &= "2"
+        End If
+        Me.cekOnce = True
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If (cekIsi()) Then
+            layarKalkulator.Text = "3"
+        Else
+            layarKalkulator.Text &= "3"
+        End If
+        Me.cekOnce = True
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If (cekIsi()) Then
+            layarKalkulator.Text = "4"
+        Else
+            layarKalkulator.Text &= "4"
+        End If
+        Me.cekOnce = True
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If (cekIsi()) Then
+            layarKalkulator.Text = "5"
+        Else
+            layarKalkulator.Text &= "5"
+        End If
+        Me.cekOnce = True
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If (cekIsi()) Then
+            layarKalkulator.Text = "6"
+        Else
+            layarKalkulator.Text &= "6"
+        End If
+        Me.cekOnce = True
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        If (cekIsi()) Then
+            layarKalkulator.Text = "7"
+        Else
+            layarKalkulator.Text &= "7"
+        End If
+        Me.cekOnce = True
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If (cekIsi()) Then
+            layarKalkulator.Text = "8"
+        Else
+            layarKalkulator.Text &= "8"
+        End If
+        Me.cekOnce = True
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        If (cekIsi()) Then
+            layarKalkulator.Text = "9"
+        Else
+            layarKalkulator.Text &= "9"
+        End If
+        Me.cekOnce = True
+    End Sub
+
+    Private Function cekIsi()
+        If (layarKalkulator.Text.Equals("0") Or Me.cek > 0) Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Private Sub clearButton_Click(sender As Object, e As EventArgs) Handles clearButton.Click
+        layarKalkulator.Text = "0"
+        Me.x = 0
+        Me.cek = 0
+    End Sub
+
+    Private Sub kurangButton_Click(sender As Object, e As EventArgs) Handles kurangButton.Click
+        If (Me.cekOnce = True) Then
+            If (Me.x = 0 Or Me.cek = 2) Then
+                Me.x += Convert.ToInt32(layarKalkulator.Text)
+            Else
+                Me.x -= Convert.ToInt32(layarKalkulator.Text)
+            End If
+            layarKalkulator.Text = Me.x.ToString
+            Me.cek = 1
+            Me.cekOnce = False
+        End If
+    End Sub
+
+    Private Sub tambahButton_Click(sender As Object, e As EventArgs) Handles tambahButton.Click
+        If (Me.cekOnce = True) Then
+            If (Me.cek = 1) Then
+                Me.x -= Convert.ToInt32(layarKalkulator.Text)
+            Else
+                Me.x += Convert.ToInt32(layarKalkulator.Text)
+            End If
+            layarKalkulator.Text = Me.x.ToString
+            Me.cek = 2
+            Me.cekOnce = False
+        End If
+    End Sub
+
+    Private Sub samadenganButton_Click(sender As Object, e As EventArgs) Handles samadenganButton.Click
+        Select Me.cek
+            Case 1
+                layarKalkulator.Text = Convert.ToString(Me.x - Convert.ToInt32(layarKalkulator.Text))
+            Case 2
+                layarKalkulator.Text = Convert.ToString(Me.x + Convert.ToInt32(layarKalkulator.Text))
+        End Select
+        Me.x = 0
+        Me.cekOnce = True
+    End Sub
+End Class
